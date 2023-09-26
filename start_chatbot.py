a585535bd4cc1340fa704986d076bd918ff60e13
ASIF <asifiqbalk99@gmail.com>
2023-09-26 21:06:35 +0530

    edit

--- start_chatbot.py
+++ start_chatbot.py
@@ -2,7 +2,7 @@ from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
-from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms import GPT4All
 import chromadb
 import time
 
